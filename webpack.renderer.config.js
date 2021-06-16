01c74621cc043337915ceb8c4f31f2f14cac994e
Melanie Mende <melanie.mende@me.com>
2021-06-16 13:15:46 +0200

    initial commit

---
+++ webpack.renderer.config.js
@@ -0,0 +1,22 @@
+const rules = require('./webpack.rules');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+  },
+};
