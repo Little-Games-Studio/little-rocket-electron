b583c59770be4989a298d942429e0c1cfd7733ad
Melanie Mende <melanie.mende@me.com>
2021-06-17 09:06:27 +0200

    assets

--- src/game.js
+++ src/game.js
@@ -2,14 +2,9 @@ import Phaser from 'phaser'
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
+    width: 600,
+    height: 800,
+    backgroundColor: '#8AA8B2',
     scene: {
         preload: preload,
         create: create
@@ -19,15 +14,16 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
+    /* this.load.setBaseURL('http://labs.phaser.io');
 
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('red', 'assets/particles/red.png'); */
 }
 
 function create() {
-    this.add.image(400, 300, 'sky');
+
+/*     this.add.image(400, 300, 'sky');
 
     var particles = this.add.particles('red');
 
@@ -43,5 +39,5 @@ function create() {
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
+    emitter.startFollow(logo); */
 }
