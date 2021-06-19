4bdc94dd55aeb487be0c7812b2a48bf9a7002ae4
Melanie Mende <melanie.mende@me.com>
2021-06-19 14:07:41 +0200

    moving clouds background

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser'
-import clouds from './assets/images/clouds.png'
+import clouds_big from './assets/images/background/clouds_big.png'
+import clouds_small from './assets/images/background/clouds_small.png'
 import rocket_png from './assets/images/rocket.png'
 import { Rocket } from './classes/rocket.ts'
 
@@ -25,18 +26,28 @@ var game = new Phaser.Game(config);
 
 function preload() {
 
-    this.load.image('clouds', clouds);
+    this.load.image('clouds_big', clouds_big);
+    this.load.image('clouds_small', clouds_small);
     this.load.spritesheet('rocket', rocket_png, { frameWidth: 50, frameHeight: 140 });
 
 }
 
 function create() {
 
-    this.add.image(10, 10, 'clouds').setOrigin(0, 0).setScale(0.95);
+    this.clouds_small = this.add.tileSprite(300, 400, 600, 1600, 'clouds_small');
+    this.clouds_big = this.add.tileSprite(300, 400, 600, 1600, 'clouds_big');
+
+    this.clouds_small.alpha = 0.3;
+    this.clouds_big.alpha = 0.6;
+    
     this.rocket = new Rocket(this, this.cameras.main.centerX, this.cameras.main.height - 100);
 
 }
 
 function update() {
+
+    this.clouds_small.tilePositionY -= 0.25;
+    this.clouds_big.tilePositionY -= 0.5;
+    
     this.rocket.update();
 }
