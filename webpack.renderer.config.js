81c501560b2196b27498dc5a164ab82f68f59a90
Melanie Mende <melanie.mende@me.com>
2021-06-20 12:41:56 +0200

    typescript config

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,16 @@
 const rules = require('./webpack.rules');
-
+/* 
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+}); */
 
 module.exports = {
-  // Put your normal webpack config below here
-  module: {
+    // Put your normal webpack config below here
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
         rules: [
             {
                 test: /\.css$/i,
@@ -16,21 +19,21 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-          },
-          {
-              test: /\.(ts|tsx)$/,
-              exclude: /(node_modules|.webpack)/,
-              use: [{
-                  loader: 'ts-loader',
-                  options: {
-                      transpileOnly: true
-                  }
-              }]
-          },
-          {
-              test: /\.mp3$/,
-              loader: 'file-loader'
-          }
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules|.webpack)/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true
+                    }
+                }]
+            },
+            {
+                test: /\.mp3$/,
+                loader: 'file-loader'
+            }
         ],
-  },
+    },
 };
